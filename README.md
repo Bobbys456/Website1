d37acbedf7935cab1198510dbe67cbc7d64909e8
Robert Sullivan <103442222+Bobbys456@users.noreply.github.com>
2022-06-27 01:01:47 +0000

    calc half implemeted no display yet

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Website1
 Basic first website for understanding both Github and HTML 
-The LInk: https://bobbys456.github.io/Website1/
+The Link: https://bobbys456.github.io/Website1/
 
 
 Example: 
--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,70 @@
-alert( 'Hello, world!' );
+let operands =  ['+', '-', '*', '/'];
+let count = 0;
+
+let currentTotal = 0; //total sum
+let inputNum = 0; //number pressed on calc
+let firstRun = true;
+
+function input(inputNum) 
+{
+    if(firstRun)
+    {
+      currentTotal = input *  (10 ^ count);
+      count++;
+    }
+
+    else
+    {
+      inputNum = input *  (10 ^ count);
+      count++;
+    }
+  alert(currentTotal);
+}
+
+function operand(operator)
+{
+  count = 0; //set count to zero to input next number in ones position 
+  
+  if(!firstRun)
+  {
+    switch(operator)
+    {
+      case('+'): 
+        addition(); 
+        break;
+      case('-'): 
+        subtraction();
+        break;
+      case('/'):
+        divide();
+        break;
+      case('*'): 
+        multiply();
+    }
+  }
+    
+  else
+  {
+    firstRun = false;
+  }
+}
+  
+function addition()
+{
+    currentTotal = currentTotal + inputNum; 
+}
+
+function multiply()
+{
+    currentTotal = currentTotal * inputNum; 
+}
+
+function subtraction()
+{
+    currentTotal = currentTotal - inputNum; 
+}
+
+function divide()
+{
+    currentTotal = currentTotal / inputNum; 
+}
